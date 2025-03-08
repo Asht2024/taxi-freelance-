ea15c905625acdf97a556cb2a2f55a6533130e2c
satyam-svg <satyammaurya9620@gmail.com>
2025-03-08 13:34:53 +0530

    feat(): fix issues

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,JSX } from "react";
 import { cn } from "../lib/utils";
 import Link from "next/link";
 import Image from "next/image";
