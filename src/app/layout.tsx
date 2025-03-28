bc6c6df1895ae639b2f8f1781c8ccaac1d857076
rahulcoder001 <124455574+rahulcoder001@users.noreply.github.com>
2025-03-28 19:56:27 +0530

    Add signin and signup routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "../../components/Navbar";
+// import { Navbar } from "../../components/Navbar";
 import { AnimatePresence } from "framer-motion";
 import Footer from "../../components/Footer";
 import AuthProvider from '../../components/AuthProvider';
@@ -21,25 +21,25 @@ export const metadata: Metadata = {
   description: "Book reliable taxi services in Gujarat with 24/7 availability",
 };
 
-const navItems = [
-  { name: "Home", link: "/" },
-  { 
-    name: "Services", 
-    link: "/Service",
-    dropdown: [
-      { name: "Local Taxi Cab Service in Ahmedabad", link: "/local-taxi-cab-service-ahmdabad" },
-      { name: "Ahmedabad to Mumbai Taxi Cab Service", link: "/ahmedabad-to-mumbai-taxi-cab-service" },
-      { name: "Rajkot to Hirasar Airport Taxi Service", link: "/rajkot-to-hirasar-cab-service" },
-      { name: "Taxi Service in Gujarat", link: "/taxi-service-in-gujarat" },
-      { name: "Taxi Service in Ahmedabad Airport", link: "/taxi-service-in-ahmedabad-airport" },
-      { name: "Rajkot Airport Taxi", link: "/rajkot-airport-taxi" },
-      { name: "Cab Service in Ahmedabad for Outstation", link: "/cab-service-in-ahmedabad-for-outstation" },
+// const navItems = [
+//   { name: "Home", link: "/" },
+//   { 
+//     name: "Services", 
+//     link: "/Service",
+//     dropdown: [
+//       { name: "Local Taxi Cab Service in Ahmedabad", link: "/local-taxi-cab-service-ahmdabad" },
+//       { name: "Ahmedabad to Mumbai Taxi Cab Service", link: "/ahmedabad-to-mumbai-taxi-cab-service" },
+//       { name: "Rajkot to Hirasar Airport Taxi Service", link: "/rajkot-to-hirasar-cab-service" },
+//       { name: "Taxi Service in Gujarat", link: "/taxi-service-in-gujarat" },
+//       { name: "Taxi Service in Ahmedabad Airport", link: "/taxi-service-in-ahmedabad-airport" },
+//       { name: "Rajkot Airport Taxi", link: "/rajkot-airport-taxi" },
+//       { name: "Cab Service in Ahmedabad for Outstation", link: "/cab-service-in-ahmedabad-for-outstation" },
 
-    ]
-  },
-  { name: "About", link: "/About" },
-  { name: "Contact", link: "/Contact" },
-];
+//     ]
+//   },
+//   { name: "About", link: "/About" },
+//   { name: "Contact", link: "/Contact" },
+// ];
 
 
 export default function RootLayout({
@@ -51,7 +51,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AuthProvider>
-          <Navbar navItems={navItems} />
+          {/* <Navbar navItems={navItems} /> */}
           <AnimatePresence mode="wait">
             {children}
           </AnimatePresence>
